5567e3f3a1739455e7a5632bb05b0931d097a79c
PepijnHogervorst <pepijnh443@gmail.com>
2020-11-07 10:20:33 +0100

    #0 Add detail.html template

--- GameLibrary/boards/views.py
+++ GameLibrary/boards/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
 
 from .models import Question
 
@@ -8,12 +9,16 @@ from .models import Question
 def index(request):
     # Get 5 latest questions
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('boards/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'boards/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'boards/detail.html', {'question': question})
 
 
 def results(request, question_id):
