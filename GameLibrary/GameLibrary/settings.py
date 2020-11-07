63cf8311f2269e65008ac5f037ff657df0718582
PepijnHogervorst <pepijnh443@gmail.com>
2020-11-07 09:33:13 +0100

    #0 Add models: question / choice
    Following tutorial: https://docs.djangoproject.com/en/3.1/intro/tutorial02/

--- GameLibrary/GameLibrary/settings.py
+++ GameLibrary/GameLibrary/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Default applications
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # User applications
+    'boards.apps.BoardsConfig',
 ]
 
 MIDDLEWARE = [
