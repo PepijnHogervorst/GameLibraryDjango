ab212d58bb556c668adb2da288329b6ca34aa64e
PepijnHogervorst <pepijnh443@gmail.com>
2020-11-10 21:52:23 +0100

    #0 Update detail view and views.py

--- GameLibrary/games/views.py
+++ GameLibrary/games/views.py
@@ -4,16 +4,17 @@ from django.template import loader
 from django.urls import reverse
 from django.views import generic
 
+from .models import Game
+
 # Create your views here.
 class IndexView(generic.ListView):
     template_name = 'games/index.html'
     context_object_name = 'latest_games_list'
 
     def get_queryset(self):
-        #return Question.objects.order_by('-pub_date')[:5]
-        return
+        return Game.objects.order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
-    # model = Question
+    model = Game
     template_name = 'games/detail.html'
