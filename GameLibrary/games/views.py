01e8e6f4c42f05d83e676a8b23af9e9c622bf08f
PepijnHogervorst <pepijnh443@gmail.com>
2020-11-10 20:32:14 +0100

    #0 Add first views

--- GameLibrary/games/views.py
+++ GameLibrary/games/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'games/index.html'
+    context_object_name = 'latest_games_list'
+
+    def get_queryset(self):
+        #return Question.objects.order_by('-pub_date')[:5]
+        return
+
+
+class DetailView(generic.DetailView):
+    # model = Question
+    template_name = 'games/detail.html'
